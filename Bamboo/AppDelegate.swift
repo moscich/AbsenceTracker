0c8d31fea6286b5bc0c75fafdb337eccc174856c
Marek Mościchowski <marek.moscichowski@miquido.com>
2020-02-10 18:57:39 +0100

    Initial Commit

---
+++ Bamboo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bamboo
+//
+//  Created by Marek Mościchowski on 10/02/2020.
+//  Copyright © 2020 Marek Mościchowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
